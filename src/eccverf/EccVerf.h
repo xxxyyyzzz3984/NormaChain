e9b1fdcb6a6917826f1e04a739590f11447b5b81
yinhaoxiao <xyh3984@gwu.edu>
2018-03-28 18:02:06 -0400

    ECIES

--- src/eccverf/EccVerf.h
+++ src/eccverf/EccVerf.h
@@ -1,10 +1,53 @@
+#ifndef ECIES_H
+#define ECIES_H
+
 #include <iostream>
-#include "cryptopp/eccrypto.h"
-#include "cryptopp/osrng.h"
-#include "cryptopp/oids.h"
+#include <string>
+#include <stdlib.h>
+#include <inttypes.h>
 
-using namespace CryptoPP;
-using namespace std;
+#include <openssl/ssl.h>
+#include <openssl/crypto.h>
+#include <openssl/err.h>
+#include <openssl/stack.h>
 
-void PrintPrivateKey(const DL_PrivateKey_EC<ECP>& key, ostream& out = cout);
+#define ECIES_CURVE  NID_secp521r1
+#define ECIES_CIPHER EVP_aes_256_cbc()
+#define ECIES_HASHER EVP_sha512()
+
+typedef struct {
+    struct {
+        uint64_t key;
+        uint64_t mac;
+        uint64_t orig;
+        uint64_t body;
+    } length;
+
+} secure_head_t;
+
+typedef char * secure_t;
+
 void test();
+EC_KEY * ecies_key_create();
+EC_GROUP * ecies_group();
+char * ecies_key_public_get_hex(EC_KEY *key);
+char * ecies_key_private_get_hex(EC_KEY *key);
+secure_t * ecies_encrypt(char *key, unsigned char *data, size_t length);
+unsigned char * ecies_decrypt(char *key, secure_t *cryptex, size_t *length);
+unsigned char * ecies_decrypt(char *key, secure_t *cryptex, size_t *length);
+EC_KEY * ecies_key_create_public_hex(char *hex);
+EC_KEY * ecies_key_create_private_hex(char *hex);
+void * ecies_key_derivation(const void *input, size_t ilen, void *output, size_t *olen);
+EC_KEY * ecies_key_create_public_octets(unsigned char *octets, size_t length);
+
+secure_t * secure_alloc(uint64_t key, uint64_t mac, uint64_t orig, uint64_t body);
+secure_t * secure_key_data(secure_t *cryptex);
+void secure_free(secure_t *cryptex);
+secure_t * secure_body_data(secure_t *cryptex);
+uint64_t secure_body_length(secure_t *cryptex);
+secure_t * secure_mac_data(secure_t *cryptex);
+uint64_t secure_mac_length(secure_t *cryptex);
+uint64_t secure_key_length(secure_t *cryptex);
+uint64_t secure_orig_length(secure_t *cryptex);
+
+#endif
